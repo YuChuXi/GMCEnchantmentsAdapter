8eddbb47a4e5d2e9501120944b407c2b42f84a68
YuChuXi <cyu245656@gmail.com>
2025-01-24 23:55:26 +0800

    init

--- src/main/java/com/yuchuxi/gmc/enchantments/adapter/Extension.java
+++ src/main/java/com/yuchuxi/gmc/enchantments/adapter/Extension.java
@@ -1,4 +1,4 @@
-package org.geyser.extension.exampleid;
+package com.yuchuxi.gmc.enchantments.adapter.Extension;
 
 import org.geysermc.event.subscribe.Subscribe;
 import org.geysermc.geyser.api.event.lifecycle.GeyserLoadResourcePacksEvent;
@@ -9,7 +9,7 @@ import java.nio.file.Path;
 
 // The main class of your extension - must implement extension, and be in the extension.yml file.
 // see https://github.com/GeyserMC/Geyser/blob/master/api/src/main/java/org/geysermc/geyser/api/extension/Extension.java for more info on available methods
-public class ExampleExtension implements Extension {
+public class Extension implements Extension {
 
     // Registering custom items/blocks, or adding resource packs (and basically all other events that are fired before Geyser initializes fully)
     // are done in their respective events. See below for an example:
